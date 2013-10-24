ef7d8d42fca34e4e38611c7cc39df8a8b2cd88e3
Leblanc Meneses <leblanc@robusthaven.com>
2013-10-24 14:17:55 -0500

    partialview implementation, ScriptExecute, ScriptQuery<T>
    - automate filling out strongly typed forms
    - added additional extensions

---
+++ SeleniumExtensions/PartialView.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using OpenQA.Selenium;
+using RobustHaven.IntegrationTests.SeleniumExtensions.Core;
+
+namespace RobustHaven.IntegrationTests.SeleniumExtensions
+{
+
+	public enum ViewModes
+	{
+		EditorTemplate,
+		DisplayTemplate
+	};
+
+	public class PartialView<TViewModel> where TViewModel : class
+	{
+		public readonly WebTestContext WebTestContext;
+		public readonly IWebDriver WebDriver;
+		public readonly IWebElement PartialElement;
+
+		public TViewModel ViewModel { get; set; }
+
+		public ViewModes ViewMode { get; set; }
+
+		public PartialView(WebTestContext webTestContext, IWebDriver webDriver, IWebElement partialElement)
+		{
+			WebTestContext = webTestContext;
+			WebDriver = webDriver;
+			PartialElement = partialElement;
+			ViewMode = ViewModes.EditorTemplate;
+		}
+
+
+		public virtual void Write()
+		{
+			Thread.Sleep(1000);
+
+			if (ViewModel == null)
+				return;
+
+			if (ViewModel is IEnumerable)
+			{
+				throw new NotSupportedException("Viewmodels that implement IEnumerable are not supported.");
+			}
+
+			var containerType = ViewModel.GetType();
+
+			var properties = containerType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(x => x.CanWrite).OrderBy(x => x.MetadataToken);
+			foreach (var property in properties)
+			{
+				bool canBeNull = property.PropertyType.IsValueType && (Nullable.GetUnderlyingType(property.PropertyType) != null);
+				TypeCode typeCode = Type.GetTypeCode(canBeNull ? Nullable.GetUnderlyingType(property.PropertyType) : property.PropertyType);
+
+				WriteForItem(property, typeCode);
+			}
+
+			Thread.Sleep(2000);
+		}
+
+		public virtual void Read()
+		{
+		}
+
+
+		protected virtual void WriteForItem(PropertyInfo property, TypeCode typeCode)
+		{
+			var containerType = ViewModel.GetType();
+			var expression = string.Format("{0}.{1}", containerType.FullName, property.Name);
+
+			var expectedValue = property.GetValue(ViewModel, null);
+
+			if (expectedValue == null)
+			{
+				return;
+			}
+
+			if (expectedValue.GetType().IsValueType)
+			{
+				if (Activator.CreateInstance(expectedValue.GetType()).ToString() == expectedValue.ToString())
+				{
+					return;
+				}
+			}
+
+			if (!string.IsNullOrEmpty(expectedValue.ToString()) )
+			{
+				var textBox = PartialElement.FindElement(By.Id(expression));
+				textBox.SendKeys(expectedValue.ToString());
+			}
+		}
+	}
+}
--- SeleniumExtensions/WebDriverExtensions.cs
+++ SeleniumExtensions/WebDriverExtensions.cs
@@ -9,21 +9,31 @@ namespace RobustHaven.IntegrationTests.SeleniumExtensions
 	{
 		public static IWebElement FocusedElement(this IWebDriver driver)
 		{
-			return (IWebElement) ((IJavaScriptExecutor) driver).ExecuteScript("return document.activeElement;");
+			return driver.ScriptQuery<IWebElement>("return document.activeElement;");
 		}
 
 		public static IWebElement FindElement(this IWebDriver driver, ISearchContext searchContext, By by,
-		                                      int timeoutInSeconds = 10)
+											  int timeoutInSeconds = 10)
 		{
 			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
 			return wait.Until(drv => searchContext.FindElement(by));
 		}
 
 		public static ReadOnlyCollection<IWebElement> FindElements(this IWebDriver driver, ISearchContext searchContext, By by,
-		                                                           int timeoutInSeconds = 10)
+																   int timeoutInSeconds = 10)
 		{
 			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
 			return wait.Until(drv => (searchContext.FindElements(by).Count > 0) ? searchContext.FindElements(by) : null);
+		}
+
+		public static void ScriptExecute(this IWebDriver driver, string script, params object[] args)
+		{
+			((IJavaScriptExecutor)driver).ExecuteScript(script, args);
+		}
+
+		public static T ScriptQuery<T>(this IWebDriver driver, string script, params object[] args)
+		{
+			return (T)((IJavaScriptExecutor)driver).ExecuteScript(script, args);
 		}
 	}
 }
