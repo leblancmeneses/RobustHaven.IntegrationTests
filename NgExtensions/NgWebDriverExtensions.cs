7590c30492432bbf48855b463a7c58660670b6cf
Leblanc Meneses <leblanc@robusthaven.com>
2013-12-16 17:10:50 -0600

    expose both scope and isolateScope variables

--- NgExtensions/NgWebDriverExtensions.cs
+++ NgExtensions/NgWebDriverExtensions.cs
@@ -8,6 +8,19 @@ namespace RobustHaven.IntegrationTests.NgExtensions
 {
 	public static class NgWebDriverExtensions
 	{
+		public static IWebElement FindElementByNgController(this IWebDriver driver, ISearchContext searchContext,
+													 string controllerName)
+		{
+			string css = string.Format("div[ng-controller='{0}']", controllerName);
+			return driver.FindElement(searchContext, By.CssSelector(css));
+		}
+
+		public static IWebElement FindElementByNgController(this IWebDriver driver, string controllerName)
+		{
+			return driver.FindElementByNgController(driver, controllerName);
+		}
+
+
 		public static T NgScope<T>(this IWebDriver driver, IWebElement element, string path)
 		{
 			if (!string.IsNullOrEmpty(path))
@@ -22,17 +35,20 @@ namespace RobustHaven.IntegrationTests.NgExtensions
 			return model;
 		}
 
-		public static IWebElement FindElementByNgController(this IWebDriver driver, ISearchContext searchContext,
-													 string controllerName)
+		public static T NgIsolateScope<T>(this IWebDriver driver, IWebElement element, string path)
 		{
-			string css = string.Format("div[ng-controller='{0}']", controllerName);
-			return driver.FindElement(searchContext, By.CssSelector(css));
+			if (!string.IsNullOrEmpty(path))
+			{
+				path = string.Format(".{0}", path);
+			}
+
+			string script = string.Format("return JSON.stringify(angular.element(arguments[0]).isolateScope(){0});", path);
+			object result = ((IJavaScriptExecutor)driver).ExecuteScript(script, element);
+			T model = JsonConvert.DeserializeObject<T>((string)result);
+
+			return model;
 		}
 
-		public static IWebElement FindElementByNgController(this IWebDriver driver, string controllerName)
-		{
-			return driver.FindElementByNgController(driver, controllerName);
-		}
 		
 		public static void NgWaitFor(this IWebDriver driver, IWebElement element, string conditionExpression, int seconds = 15)
 		{
@@ -52,6 +68,7 @@ namespace RobustHaven.IntegrationTests.NgExtensions
 
 				string script = string.Format(@"
 var scope = angular.element(arguments[0]).scope();
+var isolateScope = angular.element(arguments[0]).isolateScope();
 return {0};", conditionExpression);
 				result = driver.ScriptQuery<bool>(script, element);
 			} while (result == false);
