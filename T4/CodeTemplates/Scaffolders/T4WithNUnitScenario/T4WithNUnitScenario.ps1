4d71741ac92b1ca3b459d15a4ead68a783576371
Leblanc Meneses <leblanc@robusthaven.com>
2014-03-18 13:50:09 -0500

    IsLeaf converted to enum with HowToGenerate

--- T4/CodeTemplates/Scaffolders/T4WithNUnitScenario/T4WithNUnitScenario.ps1
+++ T4/CodeTemplates/Scaffolders/T4WithNUnitScenario/T4WithNUnitScenario.ps1
@@ -1,7 +1,8 @@
 [T4Scaffolding.Scaffolder(Description = "RobustHaven.IntegrationTests.T4WithNUnitScenario")][CmdletBinding()]
 param(
 	[parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)][string]$Scenario,
-	[switch]$IsLeaf = $true,
+	[ValidateSet('composite','leaf','test')]
+    [System.String]$HowToGenerate,
 	[string]$Project,
 	[string]$CodeLanguage,
 	[string[]]$TemplateFolders,
@@ -11,6 +12,6 @@ param(
 $namespace = (Get-Project $Project).Properties.Item("DefaultNamespace").Value
 
 Add-ProjectItemViaTemplate "_Scenario" -Template T4WithNUnitScenarioTemplate `
-	-Model @{ Namespace = $namespace; Scenario = $Scenario; IsLeaf=($IsLeaf -eq $true) } `
+	-Model @{ Namespace = $namespace; Scenario = $Scenario; HowToGenerate=$HowToGenerate } `
 	-SuccessMessage "Added Template output at {0}" `
 	-TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
